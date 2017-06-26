039fb3bde531178d014381c2c90fc99484c55915
David Halsey <dhalsey212@gmail.com>
2017-06-25 21:30:49 -0400

    Add city placeholder images

--- frontend/components/cities/cities_index_item.jsx
+++ frontend/components/cities/cities_index_item.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const CitiesIndexItem = ({ city }) => {
   return (
     <div key={city.id} className="city-photo">
-      <Link to={`cities/${city.id}`}>City Picture Here</Link>
+      <Link to={`cities/${city.id}`}>
+        <img className="city-index-image" src={window.images.sf} />      
+      </Link>
       <h2 className="city-name">{city.name}</h2>
     </div>
   );
