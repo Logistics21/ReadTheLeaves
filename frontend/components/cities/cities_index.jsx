bc9ad68bfdff61856ecef1f3b54a7e9468d010a7
David Halsey <dhalsey212@gmail.com>
2017-06-25 20:22:30 -0400

    Style city index page

--- frontend/components/cities/cities_index.jsx
+++ frontend/components/cities/cities_index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import CitiesIndexItem from './cities_index_item';
 
 class CitiesIndex extends React.Component {
@@ -12,10 +11,32 @@ class CitiesIndex extends React.Component {
     this.props.fetchCities();
   }
 
+  cities() {
+    return (
+      [
+        "Toronto", "Jersey City", "Philadelphia", "Duke University", "Seattle",
+        "Princeton", "Chattanooga", "Austin", "Harvard", "Portland", "Nairobi",
+        "UPenn", "Stanford", "Berlin", "Miami", "San Diego", "Cambridge, UK",
+        "Detroit", "Mumbai", "Paris", "Amman", "Osaka", "Tokyo", "Yoshino"
+      ]
+    );
+  }
+  cityList() {
+    const list = this.cities().map( (city, idx) => {
+      return (
+        <li className="suggest-city-list-item" key={idx}>
+          <Link to="/cities" className="suggested-city">{city}</Link>
+        </li>
+      );
+    });
+
+    return <ul className="suggest-city-list">{list}</ul>;
+  }
+
   render() {
     const cities = this.props.cities.map(city => {
       return (
-        <CitiesIndexItem city={city} />
+        <CitiesIndexItem key = {city.id} city={city} />
       );
     });
 
@@ -42,14 +63,16 @@ class CitiesIndex extends React.Component {
             <div className="cities-section-container">
               <div className="cities-partial">
                 <h2 className="cities-subtitle">We'll get here eventually ✈️</h2>
-                <p className="cities-sub-p">
+                <p className="cities-section-p">
                   For now, find and set your home city. Well make sure you
                   know when we Tea With Strangers is coming your way!
                 </p>
               </div>
               <div className="cities-section-list-container">
-                <Link className="button">DON'T SEE YOUR CITY? ADD IT HERE!</Link>
-                {this.suggestedCitiesList()}
+                <Link className="button" to="/suggestcities">DON'T SEE YOUR CITY? ADD IT HERE!</Link>
+                <div className="suggested-list">
+                  {this.cityList()}
+                </div>
               </div>
             </div>
           </div>
@@ -58,6 +81,5 @@ class CitiesIndex extends React.Component {
     );
   }
 }
-
 
 export default CitiesIndex;
