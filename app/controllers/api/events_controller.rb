23ff01e57e2ff49498517bf90af4873dfb3b2317
David <dhalsey212@gmail.com>
2017-06-28 21:27:17 -0400

    Debug signup/signin styling

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,48 +1,48 @@
 class Api::EventsController < ApplicationController
-  # def index
-  #   @city = City.find_by(id: params[:city_id])
-  #
-  #   if @city
-  #     render json: @city.events if @city.events.length > 0
-  #   else
-  #     render json: "There are no events in this city"
-  #   end
-  # end
-  #
-  # def create
-  #   @event = Event.new(event_params)
-  #   @event.host_id = current_user.id
-  #
-  #   if @event.save
-  #     render :show
-  #   else
-  #     render json: @event.errors.full_messages, status: 422
-  #   end
-  # end
-  #
-  # def show
-  #   @event = Event.find_by(id: params[:id])
-  #   @host = User.find_by(id: @event.host_id)
-  #
-  #   if @event
-  #     render :show
-  #   else
-  #     render json: "Event not found", status: 422
-  #   end
-  # end
-  #
-  # def destroy
-  #   event = Event.find_by(id: params[:id])
-  #   event.destroy
-  #   render :show
-  # end
-  #
-  # def update
-  # end
-  #
-  # private
-  #
-  # def event_params
-  #   params.require(:event).permit(:date, :address, :city_id, :host_id)
-  # end
+  def index
+    @city = City.find_by(id: params[:city_id])
+
+    if @city
+      render json: @city.events
+    else
+      render json: "There are no events in this city"
+    end
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.host_id = current_user.id
+
+    if @event.save
+      render :show
+    else
+      render json: @event.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+    @host = User.find_by(id: @event.host_id)
+
+    if @event
+      render :show
+    else
+      render json: "Event not found", status: 422
+    end
+  end
+
+  def destroy
+    event = Event.find_by(id: params[:id])
+    event.destroy
+    render :show
+  end
+
+  def update
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :address, :city_id)
+  end
 end
