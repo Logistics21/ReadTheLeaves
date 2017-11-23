ddc9e59d23a1b49578e36edbeb31f7fe49e1b093
David Halsey <dhalsey212@gmail.com>
2017-11-23 11:01:43 -0500

    Add improved seed data

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'paperclip'
-
+gem 'faker'
 gem 'aws-sdk'
 gem 'bullet', group: 'development'
 gem "figaro"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,157 @@
 City.delete_all
 
-city1 = City.create(name: "San Francisco", city_code: "SF", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/001/original/sf.jpg")
-city2 = City.create(name: "New York City", city_code: "NYC", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/002/original/nyc.jpg")
-city3 = City.create(name: "Chicago", city_code: "CHI", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/003/original/chi.jpg")
-city4 = City.create(name: "Washington D.C", city_code: "DC", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/004/original/dc.jpg")
-city5 = City.create(name: "Los Angeles", city_code: "LAX", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/005/original/lax.png")
-city6 = City.create(name: "London", city_code: "LDN", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/006/original/ldn.jpg")
+sf = City.create(name: "San Francisco", city_code: "SF", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/001/original/sf.jpg")
+nyc = City.create(name: "New York City", city_code: "NYC", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/002/original/nyc.jpg")
+chi = City.create(name: "Chicago", city_code: "CHI", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/003/original/chi.jpg")
+dc = City.create(name: "Washington D.C", city_code: "DC", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/004/original/dc.jpg")
+lax = City.create(name: "Los Angeles", city_code: "LAX", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/005/original/lax.png")
+ldn = City.create(name: "London", city_code: "LDN", image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/cities/images/000/000/006/original/ldn.jpg")
+
+
+puts "city created"
+
 
 User.delete_all
 
-user1 = User.create(first_name: "guest", last_name: "user", email: "guest@gmail.com", password: "password", is_host: true, city_id: city1.id)
-user2 = User.create(first_name: "tommy", last_name: "go", email: "test@gmail.com", password: "password", is_host: true, city_id: city1.id)
-user3 = User.create(first_name: "timmy", last_name: "to", email: "test1@gmail.com", password: "password", city_id: city1.id)
-user4 = User.create(first_name: "jimmy", last_name: "the", email: "test2@gmail.com", password: "password", city_id: city1.id)
-user5 = User.create(first_name: "kimmy", last_name: "store", email: "test3@gmail.com", password: "password", city_id: city1.id)
-user6 = User.create(first_name: "inky", last_name: "for", email: "test4@gmail.com", password: "password", is_host: true, city_id: city2.id)
-user7 = User.create(first_name: "pinky", last_name: "lots", email: "test5@gmail.com", password: "password", is_host: true, city_id: city2.id)
-user8 = User.create(first_name: "blinky", last_name: "of", email: "test6@gmail.com", password: "password", is_host: true, city_id: city2.id)
-user9 = User.create(first_name: "vince", last_name: "treats", email: "test7@gmail.com", password: "password", city_id: city3.id)
-user10 = User.create(first_name: "slimmy", last_name: "and", email: "test8@gmail.com", password: "password", city_id: city3.id)
-user11 = User.create(first_name: "clyde", last_name: "gorceries", email: "test9@gmail.com", password: "password", is_host: true, city_id: city3.id)
+# this is for guest login DO NOT DELETE
+
+guest_login = User.create!(email: "guest@gmail.com", password: "password", first_name: "Stranger",
+last_name: "Things", city_id: nyc.id, city_name: nyc.name, city_code: nyc.city_code, image: "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/users/images/000/000/013/original/mike.jpg")
+
+# this is for guest login DO NOT DELETE
+
+
+user_ids = [guest_login.id]
+
+
+1.upto(40) do |i|
+  email = Faker::Internet.email
+  password = Faker::Internet.password(8)
+  first_name = Faker::Name.name
+  city = [sf, nyc, dc, lax, ldn, chi].sample
+  i = rand(1..22)
+  image = "https://s3.us-east-2.amazonaws.com/readtheleaves1-dev/images/face#{i}.jpeg"
+
+  user = User.create!(email: email, password: password, image: image, first_name: first_name, city_id: city.id, city_name: city.name, city_code: city.city_code)
+  user_ids.push(user.id)
+end
+
+
+puts "users seeded"
 
 
 Event.delete_all
-datetime = DateTime.now
 
-Event.create(date: datetime, address: "112 Wharf Ave.",description: "Time for Tea", city_id: city1.id, host_id: user1.id)
-Event.create(date: datetime.change(day: 1), address: "1600 Pennsylvania Ave.",description: "Fight President", city_id: city1.id, host_id: user1.id)
-Event.create(date: datetime.change(day: 2), address: "1 Main St.",description: "End Corruption", city_id: city1.id, host_id: user1.id)
-Event.create(date: datetime.change(day: 2), address: "42nd St.",description: "See Hamilton", city_id: city2.id, host_id: user1.id)
-Event.create(date: datetime.change(day: 3), address: "350 Main St.",description: "Pass legislation", city_id: city2.id, host_id: user2.id)
-Event.create(date: datetime.change(day: 3), address: "Mulholland Dr.",description: "Beware David Lynch", city_id: city2.id, host_id: user2.id)
-Event.create(date: datetime.change(day: 3), address: "1 Lakeshore Dr.",description: "Tea at Cape Fear", city_id: city3.id, host_id: user2.id)
-Event.create(date: datetime.change(day: 3), address: "Anderson Blvd.",description: "Lunch at Cooper's", city_id: city3.id, host_id: user1.id)
-Event.create(date: datetime.change(day: 4), address: "12 Elm St.",description: "Don't fall Asleep", city_id: city3.id, host_id: user2.id)
-Event.create(date: datetime.change(day: 4), address: "Purgatory",description: "Meet Virgil, go on trip", city_id: city4.id, host_id: user2.id)
-Event.create(date: datetime.change(day: 4), address: "Ridgmont High",description: "Have fast times", city_id: city4.id, host_id: user2.id)
-Event.create(date: datetime.change(day: 4), address: "Compton",description: "Straight Outta", city_id: city5.id, host_id: user11.id)
-Event.create(date: datetime.change(day: 5), address: "Graceland",description: "Meet the King", city_id: city5.id, host_id: user11.id)
-Event.create(date: datetime.change(day: 5), address: "Baker St.",description: "You're cryin' now", city_id: city5.id, host_id: user11.id)
-Event.create(date: datetime.change(day: 5), address: "Get Tea",description: "It's London... what did you expect?", city_id: city6.id, host_id: user11.id)
-Event.create(date: datetime.change(day: 5), address: "Buckingham Palace",description: "Visit Mum", city_id: city6.id, host_id: user11.id)
+event_ids = []
+
+1.upto(10) do |i|
+  city = nyc
+  host_id = user_ids.sample
+  address = Faker::Address.street_address + ', New York'
+  date = Faker::Time.between(DateTime.now, DateTime.now + 30)
+
+  event = Event.create!(host_id: host_id,
+                        city_id: city.id,
+                        address: address,
+                        date: date,
+                        spots: 5)
+
+  event_ids.push(event.id)
+end
+
+
+1.upto(10) do |i|
+  city = sf
+  host_id = user_ids.sample
+  address = Faker::Address.street_address + ', San Francisco'
+  date = Faker::Time.between(DateTime.now, DateTime.now + 30)
+
+  event = Event.create!(host_id: host_id,
+                        city_id: city.id,
+                        address: address,
+                        date: date,
+                        spots: 5)
+
+  event_ids.push(event.id)
+end
+
+
+1.upto(10) do |i|
+  city = lax
+  host_id = user_ids.sample
+  address = Faker::Address.street_address +  ', Los Angeles'
+  date = Faker::Time.between(DateTime.now, DateTime.now + 30)
+
+  event = Event.create!(host_id: host_id,
+                        city_id: city.id,
+                        address: address,
+                        date: date,
+                        spots: 5)
+
+  event_ids.push(event.id)
+end
+
+
+1.upto(10) do |i|
+  city = ldn
+  host_id = user_ids.sample
+  address = Faker::Address.street_address +  ', London'
+  date = Faker::Time.between(DateTime.now, DateTime.now + 30)
+
+  event = Event.create!(host_id: host_id,
+                        city_id: city.id,
+                        address: address,
+                        date: date,
+                        spots: 5)
+
+  event_ids.push(event.id)
+end
+
+
+1.upto(10) do |i|
+  city = chi
+  host_id = user_ids.sample
+  address = Faker::Address.street_address +  ', Chicago'
+  date = Faker::Time.between(DateTime.now, DateTime.now + 30)
+
+  event = Event.create!(host_id: host_id,
+                        city_id: city.id,
+                        address: address,
+                        date: date,
+                        spots: 5)
+
+  event_ids.push(event.id)
+end
+
+
+1.upto(10) do |i|
+  city = dc
+  host_id = user_ids.sample
+  address = Faker::Address.street_address +  ', Washington, D.C'
+  date = Faker::Time.between(DateTime.now, DateTime.now + 30)
+
+  event = Event.create!(host_id: host_id,
+                        city_id: city.id,
+                        address: address,
+                        date: date,
+                        spots: 5)
+
+  event_ids.push(event.id)
+end
+
+
+puts "city created"
+
+
+Attendance.delete_all
+
+1.upto(150) do |i|
+  user_id = user_ids.sample
+  event_id = event_ids.sample
+
+  Attendance.create!(event_id: event_id, user_id: user_id) unless
+  Attendance.exists?({event_id: event_id, user_id: user_id})
+end
+
+
+puts "database seeded"
