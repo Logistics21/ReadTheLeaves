622545765f68d5522a68d5095d7a4253da6b291c
David Halsey <dhalsey212@gmail.com>
2017-11-26 15:06:05 -0500

    Fix NaN error in event item spots

--- frontend/components/events/event_item.jsx
+++ frontend/components/events/event_item.jsx
@@ -4,7 +4,6 @@ import { Link, withRouter } from 'react-router-dom';
 class EventItem extends React.Component {
   constructor(props) {
     super(props);
-
     this.handleAttend = this.handleAttend.bind(this);
     this.handleLeave = this.handleLeave.bind(this);
     this.hostCancelEvent = this.hostCancelEvent.bind(this);
